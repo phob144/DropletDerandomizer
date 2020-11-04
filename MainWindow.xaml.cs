4caa50aaf22c3257ce06a473eaf939838b835331
Phob <49253684+Phob144@users.noreply.github.com>
2020-11-04 13:00:11 +0100

    Add files for backup

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Win32;
+using DropletDerandomizer.Osu;
+using osu.Game;
+using osu.Game.Rulesets.Catch.Beatmaps;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Catch.Objects;
+using osu.Game.Rulesets.Catch;
+using System.Reflection;
+
+namespace DropletDerandomizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Filter = "Beatmap Files|*.osu",
+                Multiselect = false
+            };
+
+            bool? result = fileDialog.ShowDialog();
+
+            // null/false = error/action cancelled
+            if (result != true)
+                return;
+
+            if (Path.GetExtension(fileDialog.FileName) != ".osu")
+            {
+                MessageBox.Show("Invalid file", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            beatmapPathTextBox.Text = fileDialog.FileName;
+        }
+
+        private void derandomizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            CatchBeatmap c = BeatmapOperator.Decode(beatmapPathTextBox.Text);
+
+            MessageBox.Show(string.Join(", ", c.HitObjects[0].NestedHitObjects.Select(x => (x as CatchHitObject).X)));
+        }
+    }
+}
---
+++ Osu/BeatmapOperator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Catch;
+using osu.Game.Rulesets.Catch.Beatmaps;
+using osu.Game.Rulesets.Catch.MathUtils;
+using osu.Game.Beatmaps.Formats;
+using osu.Game.IO;
+using System.IO;
+
+// FINISH IT TOMMOROW PLEASE IT LOOKS SO PROMISING
+
+namespace DropletDerandomizer.Osu
+{
+    public static class BeatmapOperator
+    {
+        public static Beatmap Derandomize(this Beatmap beatmap)
+        {
+            CatchBeatmap catchBeatmap = GetCatchBeatmap(beatmap);
+
+            foreach (var hitObject in beatmap.HitObjects)
+            {
+
+            }
+        }
+
+        public static Beatmap Decode(string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                using (LineBufferedReader reader = new LineBufferedReader(stream))
+                {
+                    return Decoder.GetDecoder<Beatmap>(reader).Decode(reader);
+                }
+            }
+        }
+
+        private static CatchBeatmap GetCatchBeatmap(Beatmap beatmap)
+        {
+            CatchBeatmapConverter converter = new CatchBeatmapConverter(beatmap, new CatchRuleset());
+            CatchBeatmap catchBeatmap = converter.Convert() as CatchBeatmap;
+
+            // call apply defaults for each hitobject to create NestedHitObjects
+            foreach (var hitObject in catchBeatmap.HitObjects)
+            {
+                hitObject.ApplyDefaults(catchBeatmap.ControlPointInfo, catchBeatmap.BeatmapInfo.BaseDifficulty);
+            }
+
+            // apply random offsets
+            CatchBeatmapProcessor processor = new CatchBeatmapProcessor(catchBeatmap);
+            processor.PostProcess();
+
+            return catchBeatmap;
+        }
+    }
+}
